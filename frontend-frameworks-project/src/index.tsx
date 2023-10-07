19b3af7dca3b009e269eaeab96da5612df06fe9e
RodionSavenko <savenko662@gmail.com>
2023-10-07 14:40:37 +0200

    Basics

--- frontend-frameworks-project/src/index.tsx
+++ frontend-frameworks-project/src/index.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Layout from './components/Layout/Layout';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+       <Routes>
+       <Route path="/" element={<Layout />}>
+          <Route index element={<App />} />
+        </Route>
+       </Routes>
+     </BrowserRouter>
   </React.StrictMode>
 );
