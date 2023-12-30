91b9fd4438b37931d46ac2c4b9ab9538178f3ed9
Jakub <querly000@gmail.com>
2023-12-30 17:05:18 +0100

    UserProfile more info

--- frontend-frameworks-project/src/Components/Profile/UserProfile.tsx
+++ frontend-frameworks-project/src/Components/Profile/UserProfile.tsx
@@ -1,35 +1,52 @@
-import { useOutletContext } from "react-router-dom";
+import {useOutletContext} from "react-router-dom";
 import jsonApiFetch from "../../Hooks/jsonApiFetch";
-import { TUser } from "../../Model/TUser";
+import {TUser} from "../../Model/TUser";
 import JsonApiEndpointsEnum from "../../Model/JsonApiEndpointsEnum";
-import { useEffect, useState } from "react";
-import { Typography } from "@mui/material";
+import {useEffect, useState} from "react";
+import {Box, CircularProgress, Typography} from "@mui/material";
 
 const UserProfile = () => {
-  const userName: string = useOutletContext();
+    const userName: string = useOutletContext();
 
-  const [data, setData] = useState<TUser[]>([]);
-  const [userData, setUserData] = useState<TUser>();
+    const [data, setData] = useState<TUser[]>([]);
+    const [userData, setUserData] = useState<TUser>();
 
-  useEffect(() => {
-    jsonApiFetch<TUser>(
-      JsonApiEndpointsEnum.USERS,
-      `username=${userName}`,
-      setData
+    useEffect(() => {
+        jsonApiFetch<TUser>(
+            JsonApiEndpointsEnum.USERS,
+            `username=${userName}`,
+            setData
+        );
+    }, [userName]);
+
+    useEffect(() => {
+        setUserData(data[0]);
+    }, [data]);
+
+    return (
+        <Box sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center"
+        }} id="user-info-wrapper">
+            {data.length ? (
+                <>
+                    <Typography variant="h5">Name: {userData?.name}</Typography>
+                    <Typography>Email: {userData?.email}</Typography>
+                    <Typography variant="h6">Address</Typography>
+                    <Typography>Street: {userData?.address.street}</Typography>
+                    <Typography>Suite: {userData?.address.suite}</Typography>
+                    <Typography>City: {userData?.address.city}</Typography>
+                    <Typography>Zip Code: {userData?.address.zipcode}</Typography>
+                    <br/>
+                    <Typography>Phone: {userData?.phone}</Typography>
+                    <Typography>Company name: {userData?.company.name}</Typography>
+                </>
+            ) : (
+                <CircularProgress/>
+            )}
+        </Box>
     );
-  }, [userName]);
-
-  useEffect(() => {
-    setUserData(data[0]);
-  }, [data]);
-
-  return (
-    <>
-      <Typography>Name: {userData?.name}</Typography>
-      <Typography>Email: {userData?.email}</Typography>
-      <Typography>City: {userData?.address.city}</Typography>
-    </>
-  );
 };
 
 export default UserProfile;
